19c1421227d1eb37088832011ff643bdbf16f96f
jasonhama <jjhama@uw.edu>
2016-05-03 14:28:16 -0700

    noitification added

--- ClimateImpact/app/src/main/java/edu/uw/jjhama/cimateimpact/ReminderDialog.java
+++ ClimateImpact/app/src/main/java/edu/uw/jjhama/cimateimpact/ReminderDialog.java
@@ -2,9 +2,15 @@ package edu.uw.jjhama.cimateimpact;
 
 import android.app.Dialog;
 import android.app.DialogFragment;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.TaskStackBuilder;
 import android.support.v7.app.AlertDialog;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,6 +22,7 @@ import android.widget.ListView;
  */
 public class ReminderDialog extends DialogFragment{
 
+    NotificationCompat.Builder mBuilder;
     private static final String TAG = "ReminderDialog";
 
     public ReminderDialog(){
@@ -38,14 +45,44 @@ public class ReminderDialog extends DialogFragment{
                         Log.v(TAG, "Event Created");
                         // sign in the user ...
                         //Dialog f = (Dialog) dialog;
+                        Context context = getActivity();
 
+                        NotificationCompat.Builder mBuilder =
+                                new NotificationCompat.Builder(getActivity())
+                                        .setSmallIcon(R.drawable.ic_menu_send)
+                                        .setContentTitle("My notification")
+                                        .setContentText("Hello World!");
+// Creates an explicit intent for an Activity in your app
+                        Intent resultIntent = new Intent(context, Landing.class);
+
+// The stack builder object will contain an artificial back stack for the
+// started Activity.
+// This ensures that navigating backward from the Activity leads out of
+// your application to the Home screen.
+                        TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+// Adds the back stack for the Intent (but not the Intent itself)
+                        stackBuilder.addParentStack(Landing.class);
+// Adds the Intent that starts the Activity to the top of the stack
+                        stackBuilder.addNextIntent(resultIntent);
+                        PendingIntent resultPendingIntent =
+                                stackBuilder.getPendingIntent(
+                                        0,
+                                        PendingIntent.FLAG_UPDATE_CURRENT
+                                );
+                        mBuilder.setContentIntent(resultPendingIntent);
+                        NotificationManager mNotificationManager =
+                                (NotificationManager) context.getSystemService(context.NOTIFICATION_SERVICE);
+// mId allows you to update the notification later on.
+                        mNotificationManager.notify(1, mBuilder.build());
                         //EditText firstName = (EditText) f.findViewById(R.id.);
-
+                        Log.v(TAG, "Event Created!!!!!!!!!!!!!!!!!");
                         //EditText firstName = (EditText) f.findViewById(R.id.firstName);
 
 
                         //Log.v(TAG, "Submit was selected \n username: "+ );
                     }
+
+
                 })
                 .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
