16b9b74f594188455a7f1efb12de21b686b1aae0
jasonhama <jjhama@uw.edu>
2016-05-23 14:50:38 -0700

    fixed spinner color scheme on county map

--- ClimateImpact/app/src/main/java/edu/uw/jjhama/cimateimpact/CountyFragment.java
+++ ClimateImpact/app/src/main/java/edu/uw/jjhama/cimateimpact/CountyFragment.java
@@ -65,9 +65,12 @@ public class CountyFragment extends Fragment {
 
 
 
+        //ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.spinner_item,list);
+
+
         // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(),
-                R.array.planets_array, android.R.layout.simple_spinner_item);
+                R.array.planets_array, R.layout.spinner_item);
         // Specify the layout to use when the list of choices appears
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner
